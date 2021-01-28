6ab07dab2da5e266ece35a447d9afd76aed99acc
PageKallop <PageMarie78@gmail.com>
2021-01-28 13:41:30 -0600

    Created screen for macOs

--- SwiftUIForAllDevices-macOS/ContentView.swift
+++ SwiftUIForAllDevices-macOS/ContentView.swift
@@ -7,10 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DetailView {
+    let animal : Animal
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+        VStack {
+            Text(animal.image)
+                .font(.custom("Arial", size: 200))
+            Text(animal.name)
+            Text(animal.description)
+        }
+    }
+}
+
+struct AnimalList: View {
+    
+    @Binding var selectedAnimal: Animal?
+    private let animals = AnimalService.getAll()
+    var body: some View {
+        
+        List(self.animals, id: \.name) { animal in
+            VStack {
+                Text(animal.image)
+                    .font(.custom("Arial", size: 100))
+                Text(animal.name)
+            }.onTapGesture {
+                self.selectedAnimal = animal
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var selectedAnimal: Animal? = nil
+    var body: some View {
+       
+            NavigationView {
+                
+                GeometryReader { g in
+                AnimalList(selectedAnimal: self.$selectedAnimal)
+                }
+                
+                GeometryReader { g -> SwiftUIView in
+                if let animal = self.selectedAnimal {
+                    return SwiftUIView(animal: animal)
+                } else {
+                    return SwiftUIView(animal: Animal.placeholder)
+                }
+                }
+               
+            }
+            .navigationViewStyle(DoubleColumnNavigationViewStyle())
+        
     }
 }
 
